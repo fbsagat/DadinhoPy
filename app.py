78f97b616af5327ea5e84c48f31f30651babcef3
Fabio <www.fabioaugusto@hotmail.com>
2024-10-26 12:17:56 -0300

    Inicio

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+import uuid
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 socketio = SocketIO(app)
 
+users = {}
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("lobby.html")
 
 
 @socketio.on("send_message")
 def handle_send_message(data):
+    users[request.sid] = data['username']
+    print(users)
     print(f"Mensagem recebida: {data['username']}: {data['message']}")
     emit("receive_message", {"username": data["username"], "message": data["message"]}, broadcast=True)
 
